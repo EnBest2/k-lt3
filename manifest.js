2d9c60c42318452edc57301c1ae8c4bd416d14e0
EnBest2 <enbest2@gmail.com>
2025-04-14 12:48:38 +0200

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,20 @@
+{
+  "name": "Pénzügyi Napló",
+  "short_name": "Naplo",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#2F3BA2",
+  "icons": [
+    {
+      "src": "icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
